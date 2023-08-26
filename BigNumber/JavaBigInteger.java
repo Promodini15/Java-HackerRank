e076f432b3ad553348b89aa2cd75f6c115ba0da3
Promodini L <118465893+Promodini15@users.noreply.github.com>
2023-08-26 21:11:03 +0530

    Create JavaBigInteger.java

---
+++ BigNumber/JavaBigInteger.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+import java.math.BigInteger;
+public class Solution {
+    public static void main(String[] args) {
+       Scanner sc=new Scanner(System.in);
+       BigInteger bd1= new BigInteger(sc.next());
+       BigInteger bd2= new BigInteger(sc.next());
+      System.out.println(bd1.add(bd2)); 
+      System.out.println(bd1.multiply(bd2));
+ 
+    }
+}
